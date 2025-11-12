f85db345917e2d09405da5c45400490e728d1dfc
Lucky-1300 <youremail@example.com>
2025-11-12 21:33:48 +0530

    done

---
+++ arrays.py
@@ -0,0 +1,41 @@
+#Q1. Maximum and minimum of an array using minimum number of comparisonsa
+
+arr = [5, 2, 8, 1, 9, 3]
+
+min_val = arr[0]
+max_val = arr[0]
+
+for i in range(1, len(arr)):
+    current_element = arr[i]
+    if current_element > max_val:
+       max_val = current_element
+    if current_element < min_val:
+       min_val = current_element
+
+
+print("Maximum value:", max_val)
+print("Minimum value:", min_val)
+
+
+
+
+# Q2. Array Reverse
+
+arr = [10, 20, 30, 40, 50]
+start = 0
+end = len(arr) - 1
+
+while(end > start):
+
+   arr[start] , arr[end] = arr[end] , arr[start]
+
+   start += 1
+   end -= 1
+
+print(arr)
+
+
+# Q3. Maximum Subarray
+
+
+# Q4. Contains Duplicate
