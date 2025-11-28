9c7d23a5c4d9ce6504b18cbc96b175a123cb4fa8
Lucky-1300 <luckykumari42774@gmail.com>
2025-11-28 09:54:37 +0530

    done

--- conditions.py
+++ conditions.py
@@ -49,58 +49,58 @@
 
 
 #6.Check if a year is a leap year.
-year = int(input("Enter a year:"))
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap Year")
-else:
-    print("Not a Leap Year")
+# year = int(input("Enter a year:"))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
 
 
 
 
 #7.Check if a character is a vowel or consonant.
-char = input("Enter a character:")
-if char in 'aeiouAEIOU':
-    print("Vowel")
-else:
-    print("Consonant")
+# char = input("Enter a character:")
+# if char in 'aeiouAEIOU':
+#     print("Vowel")
+# else:
+#     print("Consonant")
 
 
 
 
 #8.Check if a character is an alphabet, digit, or special symbol.
-char = input("Enter a character:")
-if char.isalpha():
-    print("Alphabet")
-elif char.isdigit():
-    print("Digit")
-else:
-    print("Special Symbol")
+# char = input("Enter a character:")
+# if char.isalpha():
+#     print("Alphabet")
+# elif char.isdigit():
+#     print("Digit")
+# else:
+#     print("Special Symbol")
 
 
 
 
 #9.Find the smallest of three numbers.
-num1 = int(input("Enter a number:"))
-num2 = int(input("Enter a number:"))
-num3 = int(input("Enter a number:"))
-if num1 > num2 and num1 > num3:
-    print(num1, "is greatest")
-elif num2 > num1 and num2 > num3:
-    print(num2, "is greatest")
-else:
-    print(num3, "is greatest")
+# num1 = int(input("Enter a number:"))
+# num2 = int(input("Enter a number:"))
+# num3 = int(input("Enter a number:"))
+# if num1 > num2 and num1 > num3:
+#     print(num1, "is greatest")
+# elif num2 > num1 and num2 > num3:
+#     print(num2, "is greatest")
+# else:
+#     print(num3, "is greatest")
 
 
 
 
 
 #10.Check if a number is multiple of both 3 and 7.
-num = int(input("Enter a number:"))
-if num % 3 == 0 and num % 7 == 0:
-    print(num,"number is multiple of both 3 and 7")
-else:
-    print(num,"number is not multiple of both 3 and 7")
+# num = int(input("Enter a number:"))
+# if num % 3 == 0 and num % 7 == 0:
+#     print(num,"number is multiple of both 3 and 7")
+# else:
+#     print(num,"number is not multiple of both 3 and 7")
 
 
 
@@ -108,17 +108,17 @@ else:
 
 # 11."Find the grade of a student based on marks:
 # 90–100 → A, 80–89 → B, 70–79 → C, 60–69 → D, else → F."
-marks = int(input("Enter the marks:"))
-if marks >= 90 and marks <= 100:
-    print("A")
-elif marks >= 80 and marks <= 89:
-    print("B")
-elif marks >= 70 and marks <= 79:
-    print("C")
-elif marks >= 60 and marks <= 69:
-    print("D")
-else:
-    print("F")
+# marks = int(input("Enter the marks:"))
+# if marks >= 90 and marks <= 100:
+#     print("A")
+# elif marks >= 80 and marks <= 89:
+#     print("B")
+# elif marks >= 70 and marks <= 79:
+#     print("C")
+# elif marks >= 60 and marks <= 69:
+#     print("D")
+# else:
+#     print("F")
 
 
 
@@ -126,13 +126,13 @@ else:
 
 
 # 12.Check whether a character is uppercase, lowercase, or not an alphabet.
-char = input("Enter a character: ")
-if 'A' <= char <= 'Z':
-    print("Uppercase Alphabet")
-elif 'a' <= char <= 'z':
-    print("Lowercase Alphabet")
-else:
-    print("Not an Alphabet")
+# char = input("Enter a character: ")
+# if 'A' <= char <= 'Z':
+#     print("Uppercase Alphabet")
+# elif 'a' <= char <= 'z':
+#     print("Lowercase Alphabet")
+# else:
+#     print("Not an Alphabet")
 
 
 # 13.Determine if a year, month, and day form a valid date.
@@ -141,13 +141,13 @@ else:
 
 # 14.Take an integer input and check if its last digit is 3 or not.
 
-num = int(input("Enter a number:"))
-if num == 3:
-    print("last digit is 3")
-elif num % 10 == 3:
-    print("last digit is 3")
-else:
-    print("last digit is  not 3")
+# num = int(input("Enter a number:"))
+# if num == 3:
+#     print("last digit is 3")
+# elif num % 10 == 3:
+#     print("last digit is 3")
+# else:
+#     print("last digit is  not 3")
 
 
 
@@ -155,11 +155,11 @@ else:
 
 
 # 15.Check if a number is within a range (e.g., 10 ≤ num ≤ 50).
-num = int(input("Enter a number:"))
-if num >= 10 and num <= 50:
-    print("number is within a range of 10 to 50")
-else:
-    print("number is not in range")
+# num = int(input("Enter a number:"))
+# if num >= 10 and num <= 50:
+#     print("number is within a range of 10 to 50")
+# else:
+#     print("number is not in range")
 
 
 #16. Check if a given time (hours, minutes) is valid or not.
@@ -169,24 +169,24 @@ else:
 
 
 # 18.Check if three angles form a valid triangle.
-side1 = int(input("Enter side one :"))
-side2 = int(input("Enter side two :"))
-side3 = int(input("Enter side three :"))
-if (side1 + side2 + side3 == 180):
-    print("It's a valid triangle")
-else:
-    print("It's not a valid triangle")
+# side1 = int(input("Enter side one :"))
+# side2 = int(input("Enter side two :"))
+# side3 = int(input("Enter side three :"))
+# if (side1 + side2 + side3 == 180):
+#     print("It's a valid triangle")
+# else:
+#     print("It's not a valid triangle")
 
 
 
 #19. Check if the sum of any two numbers equals the third (like 2 + 3 = 5).
-num1 = int(input("Enter a number :"))
-num2 = int(input("Enter a number :"))
-totalNum = int(input("Enter a number :"))
-if (num1 + num2 == totalNum):
-    print("Yes , the sum of two numbers are equals to the ", totalNum)
-else:
-    print("No , the sum of two numbers are equals to the ", totalNum)
+# num1 = int(input("Enter a number :"))
+# num2 = int(input("Enter a number :"))
+# totalNum = int(input("Enter a number :"))
+# if (num1 + num2 == totalNum):
+#     print("Yes , the sum of two numbers are equals to the ", totalNum)
+# else:
+#     print("No , the sum of two numbers are equals to the ", totalNum)
 
 
 
@@ -199,12 +199,28 @@ else:
 # 80–89 → B
 # 70–79 → C
 # 60–69 → D
-
-
-
-
 # Below 60 → F
 # But if marks are exactly 100, print ""Perfect Score!""."
+# marks = int(input("Enter a number:"))
+# if marks == 100:
+#     print("Perfect Score")
+# elif marks <= 100 and marks >= 90:
+#     print("A")
+# elif marks <= 89 and marks >= 80:
+#     print("B")
+# elif marks <= 79 and marks >= 70:
+#     print("C")
+# elif marks <= 69 and marks >= 60:
+#     print("D")
+# elif marks < 60:
+#     print("F")
+
+
+
+
+
+
+
 # "2️⃣ Leap Year + Century Check
 
 
@@ -267,20 +283,27 @@ else:
 
 
 # "6️⃣ Triangle Type Validator
-
 # Given 3 sides:
-
 # Check if they form a triangle.
-
 # If yes:
-
 # If all sides equal → Equilateral
-
 # If two sides equal → Isosceles
-
 # If all sides different → Scalene
-
 # If they don’t form a valid triangle, print ""Invalid Triangle""."
+# side1 = int(input("Enter a side :"))
+# side2 = int(input("Enter a side :"))
+# side3 = int(input("Enter a side :"))
+
+
+# if(side1 + side2 + side3 != 180):
+#     print("Invalid Triangle")
+# elif (side1 == side2) and (side2 == side3):
+#     print("It is a Equilateral Triangle")
+# elif(side1 == side2) or (side1 == side3) or (side2 == side3):
+#     print("It is a Isosceles Triangle")
+# elif(side1 != side2) or (side2 != side3):
+#     print("It is a Scalene Triangle")
+
 
 
 
---
+++ loops.py
@@ -0,0 +1,129 @@
+# Print numbers from 1 to 10 using a loop.
+# for i in range(1, 11):
+#     print(i)
+
+
+
+# Print only even numbers from 1 to 20.
+for i in range(1 - 21):
+    if i % 2 == 0:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Print the sum of numbers from 1 to N.
+
+
+
+
+
+
+
+
+
+
+
+# Print the multiplication table of a given number.
+
+
+
+
+
+
+
+
+
+
+
+
+# Count how many digits are in a number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Print the factorial of a number (using a loop).
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Reverse a number (e.g., 123 → 321).
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Print the first N natural numbers in reverse order.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Print all numbers divisible by 5 between 1 and 100.
+
+
+
+
+
+
+
+
+
+
+
+
+
+# "Print a pattern:
+# *
+# **
+# ***
+# ****
